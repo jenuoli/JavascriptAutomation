eb199ca3fe2899ad9c5fbcdcca537c68b504f2c0
Jenu Oli <jenuoil123@gmail.com>
2024-08-30 10:53:30 +0545

    JavaScript

---
+++ NotOpreator.js
@@ -0,0 +1,18 @@
+// used to make false and it is boolean
+
+let a=4
+console.log(a)
+console.log(typeof(a))
+let b=234.6
+console.log(typeof(b))
+
+var c="Jenu Oli"
+console.log(typeof(c))
+
+let required=true
+console.log(typeof(required))
+
+//let c= a+b (it did not work//we cannot redeclare variable with let keyword but possible with var)
+var c= a+b
+console.log(c)
+console.log(!required)
